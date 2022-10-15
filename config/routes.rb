de31e4e3d5b04c80395f1470ff6c097579f5c8f7
Lucas Luz <lucas@calazansluz.com>
2022-10-15 13:42:05 +0200

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+# config/routes.rb
+Rails.application.routes.draw do
+  get 'pages/home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  root to: "pages#home"
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
